66b38bda0bb4a2e67d3ead79dc85c4e535dc01c0
SanjeevGwachha <sanjeev@oneloopstudio.com>
2024-07-22 17:57:34 +0545

    working on webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
 const port = process.env.PORT || 8080;
 const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
-    filename: "main.[fullhash]js",
+    filename: "main.[fullhash].js",
   },
-  module:{
-    ruels:[
-        {
-            test: /\/(js|jsx)$/,
-        }
-    ]
-  }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins:[
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    historyApiFallback: true,
+    open: true,
+  },
 };
